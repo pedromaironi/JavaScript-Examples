f433bcdbd16a607b162c2378b0dd9527d9130b68
PedroDeveloper <pedrom_t@hotmail.com>
2020-04-28 14:03:41 -0400

    Bucles: For of

--- src/main.js
+++ src/main.js
@@ -1,35 +1,15 @@
 console.log('\tWELCOME');
 
 
-/* Arrays: */
+/* Bucles de ecmascript: */
 /* 
-    Eliminar un elemento
-    .shift() - Elimina el primer elemento del array y dvuelve ese elemento
-    El método pop() elimina el último elemento de un array y lo devuelve. 
-    Este método cambia la longitud del array.
-    .push() - agrega elementos a el array
-    .unshift() - agrega elementos a el array y retorna su longitud
-    .indexOf() - devuelve el indice del valor encontrado y si no lo encuentra devuelve -1;
-    .lasIdexOf() - devuelve el ultimo index que coincide con el valor especificado 
-    sino -1 si no es econtrado
-    .reverse() - voltear el array
+    Bucle For of : Podemos recorrer un arreglo sin necesidad de inicializar una variable
+    para realizar un incremento o dar un numero x de vueltas. Esto simplifica el metodo tradicional
 */
 
+Array = ['Pedro', 'Toribio'];
 
-
-var array = [1,2,3,4];
-var newArray = [];
-var funcion = (array) => { 
-    for(var i = 0 ; i < array.length ; i++){
-        if( i < array.length ){
-            let num = array.pop(array[i]);
-            i-=1;
-            newArray.push(num);
-        } 
-    }
-    return newArray;
+for(let propiedades of Array){
+    console.log(propiedades);
 }
 
-console.log('Array: ', array);
-
-console.log('newArray: ', funcion(array));
