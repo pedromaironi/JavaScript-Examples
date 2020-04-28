b1b113e4e18f79ebcf2a21ea0b8c71561545100f
PedroDeveloper <pedrom_t@hotmail.com>
2020-04-28 14:19:56 -0400

    Bucle: For in

--- src/main.js
+++ src/main.js
@@ -4,21 +4,11 @@ console.log('\tWELCOME');
 /* Bucles de ecmascript: */
 /* 
     Bucle For of : Podemos recorrer un arreglo sin necesidad de inicializar una variable
-    para realizar un incremento o dar un numero x de vueltas. Esto simplifica el metodo tradicional
+    para realizar un incremento o dar un numero x de vueltas. Esto simplifica el metodo tradiciona
+    bucle For in : devuelve el index de los datos que hay en el array
 */
 
-Array = [
-    {
-    name =  'pedro'
-    }
-    ,
-    {
-    lastName = 'Toribio'
-    }
-];
-
-for(let propiedades of Array){
-    console.log(propiedades);
+array = [1,2,3,4];
+for(let items in array){
+    console.log(items);
 }
-
-
