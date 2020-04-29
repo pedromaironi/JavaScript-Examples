5584a442afe7865d66fbe58d52c78acbacf5dd80
PedroDeveloper <pedrom_t@hotmail.com>
2020-04-29 19:37:53 -0400

    Callbacks

--- src/main.js
+++ src/main.js
@@ -1,50 +1,44 @@
 console.log('\tWELCOME');
 
-/* Spread operator */
-
-/* const addNumbers = ( a,b,c ) =>{
-    console.log(a+b+c);
-}
-
-let numbersToAdd = [1,2,3];
-addNumbers(...numbersToAdd);
-
-console.log(addNumbers); */
 /* 
-Push
-const Array = ['pedro'];
-const Array = ['pedro'];
-const Array2 = ['Toribio'];
-
-for(let items in Array2){
-    Array.push(Array2[items]);
-    console.log(items);
-}
-
-console.log(Array);
- */
-/* 
- Copiar array a otro
-const Array = ['pedro'];
-const Array2 = [...Array];
-console.log(Array);
- */
-/* 
-
-const Array = ['pedro'];
-const Array2 = ['Toribio'];
-
-Mala forma
-let concat = Array.concat(Array2);
-console.log(concat);
-
-Otra forma 
-let concat = [...Array1, ...Array2]; */
-
-/* 
-const restParams = (...numbers)=> {
-    console.log(numbers);
-}
-
-restParams(1,2,2,2,2,2,2,2,2,2,2,2); 
+    * Callbacks
+TODO Un callback es una funcion que se ejecuta a travez de otra funcion. los callbacks no son async osea que se ejecutan por orden
+    ! cb = callback
 */
+
+const getUser = (id,cb) => {
+    const user ={
+        name : 'Pedro',
+        id: id
+    }
+    cb(user);
+}
+
+getUser( 1,(user, err) => {
+    if(err){
+        return console.log(err);
+    } else {
+        console.log(`User name: ${user.name}`);
+        
+    }
+})
+
+
+
+
+
+
+
+
+
+const saludo = (id, cb) => {
+    const person = {
+        name : 'Pedro',
+        id 
+    }
+}
+
+saludo(1, (person, errores) => {
+    if(errores) return 'Error indevido';
+    console.log(person.name);
+})
