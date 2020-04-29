b41bcc339704bef6f6ad9a3cc72b2e130c6d81eb
PedroDeveloper <pedrom_t@hotmail.com>
2020-04-28 21:11:50 -0400

    Nuevamente practicando methods de los Arrays(sort y from)

--- src/main.js
+++ src/main.js
@@ -1,23 +1,37 @@
 console.log('\tWELCOME');
 
 
-/* Bucles de ecmascript: */
+/* Fundamentos de Arrays: */
 /* 
-    Bucle For of : Podemos recorrer un arreglo sin necesidad de inicializar una variable
-    para realizar un incremento o dar un numero x de vueltas. Esto simplifica el metodo tradicional
-    For in: devuelve el nombre de la key o index de el array u objeto
+    |- .from(iterable) - Convierte en Array el elemento iterable ELEMENTOS STRING
+    |- .sort([callback]) - Ordena los elementos de un array alfabeticamente(valor Unicode)
+    si le pasamos un callback los ordena en funcion del algoritmo que le pasemos.
+    |- .forEach(callba ck(currentValue, [index]))) ejecuta la funcion indicada una vez por cada elemento del arra
+
 */
 
-Person = 
-{
-    name: 'Pedro', 
-    lastName: 'Toribio', 
-    customer : [
-        pedro = 'pedro'
-    ]   
-};
+/*
+From 
+Arreglo = "1234";
+console.log(Array.from(Arreglo));
+[ '1', '2', '3', '4' ]
+ */
 
-for(const propiedades in Person){
-    console.log(Person[propiedades]);
+ /* sort 
+
+const Arreglo = [2,3,4,5]
+console.log(Arreglo.sort( (a,b) =>b-a));
+
+menorMayor(a,b){
+    if (a-b < 0) return -1;
+    if(a-b > 0) return 1;
+    if(a--b) return 0;
 }
 
+MayorMenor(a,b){
+    if(b-a < 0) return -1;
+    if(b-a > 0) return 1;
+    if(b==a) return 0;
+}
+
+*/
