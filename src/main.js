a7f47c4cce6b433bcaf1fd29b850a875a403c552
PedroDeveloper <pedrom_t@hotmail.com>
2020-05-02 14:51:29 -0400

    Fetch teoria | Agregando teoria de peticiones fetch"

--- src/main.js
+++ src/main.js
@@ -1,64 +1,20 @@
+/*  
+    ! Fetch Api
+    * Proporciona una interfaz javascript para acceder y manipular partes del canal HTTP. como
+    * peticiones y respuestas.
+    * También provee un metodo global fetch() que proporciona una forma facil y logica de obtener 
+    * recursos de forma asincrona por la red
+    * Esta basadp en promesas, por lo cual tiene un response y un reject internos
+    ?       Response tiene varios metodos
+    ?       ArrayBuffer(): Arvhibos binarios en bruto(mp3,pdf, jpg, etc). Se utiliza cuando se necesita maipular el contenido del archivo
+    ?       blob(): crea un clon de un objeto de respuesta, identico en todos los sentidos, pero almacenado en una variable diferente
+    ?       formData(): Se utiliza para leer los objetos formData
+    ?       json(): Convierte los archivos json en un objeto de JavaScript.
+    ?       text(): Se utiliza cuando queremos leer un archivo de texto. Siempre se codifica en UTF-8
+*/
 
+const button = document.getElementById('button');
 
-const person = [
-    {
-        name: 'Pedro',
-        id: 1
-    },{
-        name: 'Laura',
-        id: 2
-    },{
-        name: 'Deisbelle'
-        ,id:3
-    }
-]
-
-const emails = [
-    {
-        email: 'pedro@hotmail.com',
-        id: 1
-    },{
-        email: 'Laura@hotmail.com',
-        id: 2
-    },{
-        email: 'Debby@hotmail.com',
-        id: 3
-    }
-]
-let promise;
-const getPerson = (id) => {
-    const persona = person.find(persona => persona.id == id )
-    return promise = new Promise ( (resolve, reject) => {
-        if(!persona) reject(`Doesn't exist an user with id ${id}`);
-        else resolve(persona);
-    });
-}
+button.addEventListener('click', () => {
     
-
-const getEmail = (user) => {
-    const email = emails.find(email => email.id = user.id );
-    return promise = new Promise( (resolve, reject ) => {
-        if(!user) reject(`${user.name} hasn't email`);
-        else resolve({
-            id: user.id,
-            name: user.name,
-            email: email.email
-        })
-    })
-}
-
-// * Como no se puede acceder de forma directa a la propiedad .value de las promesas utilizamos la propiedad 
-// * then para acceder a estos datos encapsulados
-getPerson(5).then( persona => getEmail(persona))
-            .then( data => console.log(data))
-            .catch( err => console.log(err));
-            // ! Catch para manejar errores y evitar uncaught exception
-
-
-
-/* if(err) console.log(`El error es: ${err}`);
-console.log(persona);
-getEmail(1, (err, email) => {
-    if(err) console.log(`El error es: ${err}`);
-    console.log(`La persona ${persona.name} posee el id:1 con el email ${email.email}`);
-}); */
+})
